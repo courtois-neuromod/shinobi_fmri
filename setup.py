2dcecbcd499a74eaae91f3cde5181c467cab8875
hyruuk <yharel109@gmail.com>
2021-05-24 17:26:16 -0400

    [DATALAD] Recorded changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='fMRI analysis of the cneuromod.shinobi dataset',
+    author='Yann Harel',
+    license='MIT',
+)
