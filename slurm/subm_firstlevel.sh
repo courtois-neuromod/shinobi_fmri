bef1b743490af359944ff855d4653f02e82f433c
Hyruuk <yharel109@gmail.com>
2021-08-24 15:52:52 -0400

    moved a few things

---
+++ slurm/subm_firstlevel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=rrg-pbellec
+#SBATCH --time=12:00:00
+#SBATCH --job-name=shinobi_firstlevel_fmricontrast
+#SBATCH --mem=128G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+
+module load python/3.8.0
+
+/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/models/fmri_sessionlevel_contrast.py
