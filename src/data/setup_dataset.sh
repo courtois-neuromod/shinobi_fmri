51ba5b706ce6b264bb552f46d57366f0d051eca0
Hyruuk <yharel109@gmail.com>
2021-09-07 14:28:47 -0400

    updated setup script

--- src/data/setup_dataset.sh
+++ src/data/setup_dataset.sh
@@ -1,1 +1,13 @@
-./data/shinobi_beh/src/data/get_dataset.sh
+cd ./data/shinobi_behav
+datalad get ./
+cd ./data/shinobi
+git checkout event_files
+datalad get ./
+cd ..
+scp -r yharel@elm.criugm.qc.ca:/data/neuromod/DATA/games/shinobi_beh ./
+
+# Additional lines to setup gym environment
+cd shinobi/stimuli
+datalad get ./
+conda activate shinobi_env
+python3 -m retro.import ./data/shinobi/stimuli/ShinobiIIIReturnOfTheNinjaMaster-Genesis
