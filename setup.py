511e6a9c1159a6765639297abe22aeabe42daab0
Hyruuk <yharel109@gmail.com>
2021-09-07 16:43:39 -0400

    changed src for shinobi_fmri

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='shinobi_fmri',
     packages=find_packages(),
     version='0.1.0',
     description='fMRI analysis of the cneuromod.shinobi dataset',
--- shinobi_fmri/data/setup_dataset.sh
+++ shinobi_fmri/data/setup_dataset.sh
@@ -8,6 +8,13 @@ scp -r yharel@elm.criugm.qc.ca:/data/neuromod/DATA/games/shinobi_beh ./
 
 # Additional lines to setup gym environment
 cd shinobi/stimuli
+export AWS_ACCESS_KEY_ID=yharel  AWS_SECRET_ACCESS_KEY=qrVwnh1NYZfpCHtTL7glEfdY
 datalad get ./
 conda activate shinobi_env
-python3 -m retro.import ./data/shinobi/stimuli/ShinobiIIIReturnOfTheNinjaMaster-Genesis
+python3 -m retro.import ./ShinobiIIIReturnOfTheNinjaMaster-Genesis
+cd ../../..
+pip install -r requirements.txt
+python setup.py install
+cd ../..
+pip install -r requirements.txt
+python setup.py install
