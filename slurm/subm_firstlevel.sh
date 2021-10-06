bdf6696d0e4ee93f67a05f282f0cf519e39532f8
Hyruuk <yharel109@gmail.com>
2021-10-06 14:33:42 -0400

    replaced butterworth filtering by clean_img

--- slurm/subm_firstlevel.sh
+++ slurm/subm_firstlevel.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_firstlevel_fmricontrast
-#SBATCH --mem=128G
+#SBATCH --mem=256G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
 module load python/3.8.0
 
-/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/models/fmri_sessionlevel_contrast.py
+/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/NOCONF_glm_sessionlevel_contrast.py -s 01
