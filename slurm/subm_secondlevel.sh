07082f17158a3bebabf0179c3eb74de309eb8361
Hyruuk <yharel109@gmail.com>
2021-10-06 20:47:33 -0400

    fixed crash on empty ses

--- slurm/subm_secondlevel.sh
+++ slurm/subm_secondlevel.sh
@@ -6,6 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
-module load python/3.8.0
-
 /lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/models/fmri_subjectlevel_contrast.py
