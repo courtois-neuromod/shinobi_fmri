7763b288ea55039005b5fdf99da75b63184788a6
Hyruuk <yharel109@gmail.com>
2021-10-26 16:11:35 -0400

    updated slurm file

--- slurm/subm_secondlevel.sh
+++ slurm/subm_secondlevel.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
-/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/models/fmri_subjectlevel_contrast.py
+/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/glm_subjectlevel_contrast.py
