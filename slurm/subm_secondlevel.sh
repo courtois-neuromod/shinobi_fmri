8ab913bf26b9c93baa08b61ff1731e251eeb96cb
Hyruuk <yharel109@gmail.com>
2021-10-27 11:30:20 -0400

    increased mem alloc

--- slurm/subm_secondlevel.sh
+++ slurm/subm_secondlevel.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_secondlevel_fmricontrast
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
