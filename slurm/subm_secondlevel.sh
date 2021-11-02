9d3e4fc13ec69ca575bc719c52d3c2162a1635f4
Hyruuk <yharel109@gmail.com>
2021-11-02 14:36:34 -0400

    changed env

--- slurm/subm_secondlevel.sh
+++ slurm/subm_secondlevel.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
-/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/glm_subjectlevel_contrast.py
+/home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/glm_subjectlevel_contrast.py
