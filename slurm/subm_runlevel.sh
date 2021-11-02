c0612880cc14b64f91c8a1cb42d38db62844959e
Hyruuk <yharel109@gmail.com>
2021-11-02 14:29:39 -0400

    decreased alloc

--- slurm/subm_runlevel.sh
+++ slurm/subm_runlevel.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_firstlevel_fmricontrast
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=8
 
 /home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/glm_runlevel_contrast.py -s $1 -c $2
