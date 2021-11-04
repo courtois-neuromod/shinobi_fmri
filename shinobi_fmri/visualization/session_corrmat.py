d3546956a81bb9e744917fd6f0e0f3a537b46c42
Hyruuk <yharel109@gmail.com>
2021-11-04 15:31:11 -0400

    updated corrmat script for run-level

--- shinobi_fmri/visualization/session_corrmat.py
+++ shinobi_fmri/visualization/session_corrmat.py
@@ -6,13 +6,20 @@ import os
 import numpy as np
 import seaborn as sbn
 from nilearn.input_data import NiftiMasker
+import psutil
+
+def mem_used():
+    tot = psutil.virtual_memory().total / 10**9
+    used = psutil.virtual_memory().used / 10**9
+    print('Memory currently used : {} Go/{} Go'.format(used, tot))
+
 
 ## Set constants
 contrasts = ['HealthLoss', 'Jump', 'Hit']
-subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
+#subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
 
-path_to_data = '/home/hyruuk/GitHub/neuromod/shinobi_fmri/data/'
-
+#path_to_data = '/home/hyruuk/scratch/neuromod/shinobi_data/'
+mem_used()
 
 # load data
 maps = []
@@ -22,49 +29,59 @@ sess_arr = []
 run_arr = []
 cond_arr = []
 fnames = []
+mapnames = []
 for contrast in contrasts:
     files = os.listdir(path_to_data + 'processed/cmaps/run-level/{}/'.format(contrast))
-    for file in files[]:
+    for file in files[:3]:
         fpath = path_to_data + 'processed/cmaps/run-level/{}/'.format(contrast) + file
         sub = file[0:6]
         ses = file[7:14]
         run = file[20]
         print('run : '+ file)
-        raw_dpath = '/media/storage/neuromod/shinobi_data/shinobi/derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'.format(sub, ses, sub, ses, run)
+        raw_dpath = path_to_data + 'shinobi/derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'.format(sub, ses, sub, ses, run)
         niimap = image.load_img(fpath)
-        maps.append(niimap.get_fdata())
+        maps.append(niimap)
         subj_arr.append(sub)
         sess_arr.append(ses)
         run_arr.append(run)
         cond_arr.append(contrast)
         #raw_data.append(image.concat_imgs(raw_dpath))
-        fnames.append(fpath)
+        fnames.append(raw_dpath)
+        mapnames.append(fpath)
+
+
+print('loading done')
+mem_used()
+
 
 # Create common masker
 masker = NiftiMasker()
 masker.fit(fnames)
-
+print('masks done')
+mem_used()
 masked_maps = []
 for map in maps:
     masked_maps.append(masker.transform(map))
+print('transforms done')
+mem_used()
 
-maps = np.array(maps)
-corr_map = np.zeros(maps.shape[1:])
-sess_idx = [x for x in range(maps.shape[0])]
+maps = np.array(masked_maps).squeeze()
+
+#sess_idx = [x for x in range(maps.shape[0])]
 
 # compute corrcoefs
 corr_matrix = np.zeros((len(maps), len(maps)))
 for i in range(len(maps)):
     for j in range(len(maps)):
-        # if j>i
-        imap = maps[i,:,:,:].squeeze().flatten()
+        #if j>i:
+        imap = maps[i]
         imap_trim = np.array([x for x in imap if x != 0])
-        jmap = maps[j,:,:,:].squeeze().flatten()
+        jmap = maps[j]
         jmap_trim = np.array([x for x in jmap if x != 0])
         print('i : {}, j : {}'.format(i,j))
-        print('n zeros in jth map : {}'.format(np.unique(np.unique(jmap, return_counts=True)[1])[1]))
-        print('isize : {}, jsize : {}'.format(imap_trim.shape, jmap_trim.shape))
-        corr_matrix[i,j] = np.corrcoef(imap_trim, jmap_trim)[0,1]
+        coeff = np.corrcoef(imap_trim, jmap_trim)[0,1]
+        corr_matrix[i,j] = coeff
+
 
 dict = {'corr_matrix': corr_matrix,
         'fnames': fnames,
@@ -75,17 +92,16 @@ dict = {'corr_matrix': corr_matrix,
 
 results_path = '/home/hyruuk/scratch/neuromod/shinobi_data/processed/cmaps/runlevel_maps_corrs.pkl'
 with open(results_path, 'wb') as f:
-    pickle.dump(f, dict)
+    pickle.dump(dict, f)
 
-'''
+
 fig, ax = plt.subplots(figsize=(15,15))
 mask = np.zeros_like(corr_matrix)
 mask[np.triu_indices_from(mask)] = True
-sbn.heatmap(corr_matrix, annot=True, xticklabels=names, yticklabels=names, ax=ax, cbar=False, mask=mask, annot_kws={"size":14, ''})
+sbn.heatmap(corr_matrix, annot=True, xticklabels=mapnames, yticklabels=mapnames, ax=ax, cbar=False, mask=mask, annot_kws={"size":14})
 plt.yticks(fontsize=13)
 plt.xticks(fontsize=13)
 
-if not os.path.isdir(path_to_data + './reports/figures/session_plots'):
-    os.mkdir(path_to_data + './reports/figures/session_plots')
-fig.savefig('./reports/figures/session-level_{}.png'.format(contrast))
-'''
+if not os.path.isdir(figures_path + '/run_corrmats/'):
+    os.mkdir(figures_path + '/run_corrmats/')
+fig.savefig(figures_path + '/run_corrmats/run-level_corrmat.png')
--- slurm/subm_corrmat.sh
+++ slurm/subm_corrmat.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_corrmat
-#SBATCH --mem=256G
+#SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
