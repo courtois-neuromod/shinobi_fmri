348fa42b48a24f35d1aad9279276f00193acce7a
Hyruuk <yharel109@gmail.com>
2021-12-11 16:01:57 -0500

    adjusted slurm files

--- slurm/batch_subjlevel.sh
+++ slurm/batch_subjlevel.sh
@@ -1,5 +1,7 @@
+for FROM in run session; do
 for COND in Jump Hit Kill; do
 for SUB in 01 02 04 06; do
-sbatch ./slurm/subm_subjlevel.sh $SUB $COND
+sbatch ./slurm/subm_subjlevel.sh $SUB $COND $FROM
+done
 done
 done
