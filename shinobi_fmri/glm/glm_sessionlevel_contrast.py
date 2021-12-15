c22db19538b1655eeba90e2cdb82d43987e6b0a7
Hyruuk <yharel109@gmail.com>
2021-12-15 16:11:48 -0500

    added surface and slice views

--- shinobi_fmri/glm/glm_sessionlevel_contrast.py
+++ shinobi_fmri/glm/glm_sessionlevel_contrast.py
@@ -136,6 +136,33 @@ for ses in sorted(seslist): #['ses-001', 'ses-002', 'ses-003', 'ses-004']:
                                           output_type='z_score')
         z_map.to_filename(z_map_fname)
         print('z_map saved')
+
+
+        ### Plots
+        run = '2'
+        fmri_fname = op.join(
+            path_to_data,
+            "shinobi",
+            "derivatives",
+            "fmriprep-20.2lts",
+            "fmriprep",
+            #'/lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/shinobi/',
+            sub,
+            ses,
+            "func",
+            f"{sub}_{ses}_task-shinobi_run-{run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz",
+        )
+        fmri_img = nb.load(fmri_fname)
+        # Make an average
+        bg_img = image.mean_img(fmri_img)
+
+        # Raw map
+        # Plot surface
+        plot_img_on_surf(z_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast,f'{sub}_{ses}_{contrast}.png'))
+        plot_stat_map(z_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast, f'{sub}_{ses}_{contrast}_slices.png'))
+
+
+        # Report
         report = fmri_glm.generate_report(contrasts=[contrast])
         report.save_as_html(figures_path + '/session-level-allregs' + '/{}/{}_{}_{}_flm.html'.format(contrast, sub, ses, contrast))
 
@@ -146,11 +173,16 @@ for ses in sorted(seslist): #['ses-001', 'ses-002', 'ses-003', 'ses-004']:
         # save images
         print('Generating views')
         view = plotting.view_img(clean_map, threshold=3, title='{} (FDR<0.05), Noyaux > 10 voxels'.format(contrast))
-        view.save_as_html(figures_path + '/session-level-allregs/' + '/{}/{}_{}_{}_flm_FDRcluster_fwhm5.html'.format(sub, ses, contrast))
+        view.save_as_html(figures_path + '/session-level-allregs/' + '{}/{}_{}_{}_flm_FDRcluster_fwhm5.html'.format(sub, ses, contrast))
+        plot_img_on_surf(clean_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast,f'{sub}_{ses}_{contrast}_FDR.png'))
+        plot_stat_map(clean_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast, f'{sub}_{ses}_{contrast}_slices_FDR.png'))
         # save also uncorrected map
         view = plotting.view_img(uncorr_map, threshold=3, title='{} (p<0.001), uncorr'.format(contrast))
-        view.save_as_html(figures_path + '/session-level-allregs/' + '/{}/{}_{}_{}_flm_uncorr_fwhm5.html'.format(sub, ses, contrast))
-        0/0
+        view.save_as_html(figures_path + '/session-level-allregs/' + '{}/{}_{}_{}_flm_uncorr_fwhm5.html'.format(sub, ses, contrast))
+        plot_img_on_surf(uncorr_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast, f'{sub}_{ses}_{contrast}_uncorr.png'))
+        plot_stat_map(uncorr_map, bg_img=bg_img, output_file=op.join(shinobi_behav.figures_path, 'subject-level-from-session', contrast, f'{sub}_{ses}_{contrast}_slices_uncorr.png'))
+
+
     except Exception as e:
         print(e)
         print('Session map not computed.')
