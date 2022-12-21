a55dddf96e2d52fd767598f76f3d4cbdd6646535
Yann Harel <yharel109@gmail.com>
2022-12-21 11:18:13 -0500

    .

--- shinobi_fmri/data/setup_dataset.sh
+++ shinobi_fmri/data/setup_dataset.sh
@@ -8,7 +8,7 @@ scp -r yharel@elm.criugm.qc.ca:/data/neuromod/DATA/games/shinobi_beh ./
 
 # Additional lines to setup gym environment
 cd shinobi/stimuli
-export AWS_ACCESS_KEY_ID=yharel  AWS_SECRET_ACCESS_KEY=qrVwnh1NYZfpCHtTL7glEfdY
+# export aws etc...
 datalad get ./
 conda activate shinobi_env
 python3 -m retro.import ./ShinobiIIIReturnOfTheNinjaMaster-Genesis
