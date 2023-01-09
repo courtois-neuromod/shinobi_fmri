08f72cf4e84edd7ba6fdb9dff92a2ff02e965e9f
Yann Harel <yharel109@gmail.com>
2023-01-09 12:28:35 -0500

    updated slurm files for ses level

--- slurm/subm_sesslevel.sh
+++ slurm/subm_sesslevel.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_seslevel.py -s $1 -ses $2
+/home/hyruuk/python_envs/shinobi_env/bin/python /project/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_seslevel.py -s $1 -ses $2
