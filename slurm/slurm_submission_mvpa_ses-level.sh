f65d31367333480f7433c1a36bf6b55c9529e81e
Yann Harel <yharel109@gmail.com>
2023-03-11 02:15:12 -0500

    created slurm for mvpa + cleaned slurmfodler

---
+++ slurm/slurm_submission_mvpa_ses-level.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-pbellec
+#SBATCH --time=12:00:00
+#SBATCH --job-name=shi_mvpa_seslvl
+#SBATCH --mem=128G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+
+/home/hyruuk/python_envs/shinobi_env/bin/python /project/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/notebooks/test_mvpa.py
