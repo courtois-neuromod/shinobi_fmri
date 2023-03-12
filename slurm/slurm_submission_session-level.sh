a2a93cf4ac0241709827fff43da7285d6a7e3542
Yann Harel <yharel109@gmail.com>
2023-03-11 20:13:55 -0500

    corrected paths in slurm files

--- slurm/slurm_submission_session-level.sh
+++ slurm/slurm_submission_session-level.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=rrg-pbellec
+#SBATCH --account=def-pbellec
 #SBATCH --time=4:00:00
 #SBATCH --job-name=shi_sesslevel
 #SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /project/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_session_level.py --subject $1 --session $2
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_session_level.py --subject $1 --session $2
