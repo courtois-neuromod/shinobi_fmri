79c8f71a56e70c0fa9394b9e60be2cca7928d79f
Yann Harel <yharel109@gmail.com>
2023-03-22 14:50:44 -0400

    fixed subm

--- slurm/slurm_submission_run-level.sh
+++ slurm/slurm_submission_run-level.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-pbellec
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=shi_runlevel
 #SBATCH --mem=96G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_run_level.py -s $1 -ses $2
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/glm/compute_run_level.py -s $1 -ses $2
