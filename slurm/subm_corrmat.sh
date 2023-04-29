7107aafda03afc251170c0971f1e448e6fd76ea1
Yann Harel <yharel109@gmail.com>
2023-04-29 12:39:06 -0700

    adjusted slurmfile

--- slurm/subm_corrmat.sh
+++ slurm/subm_corrmat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=rrg-pbellec
+#SBATCH --account=def-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_corrmat
 #SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/visualization/session_corrmat.py
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/visualization/session_corrmat.py
