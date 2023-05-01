29373121d9fd84589aef04a0501f7264b4a849cd
Yann Harel <yharel109@gmail.com>
2023-04-30 21:33:54 -0700

    updated slurm subm for seslevel

--- slurm/subm_viz-sesslevel.sh
+++ slurm/subm_viz-sesslevel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=rrg-pbellec
-#SBATCH --time=12:00:00
+#SBATCH --account=def-pbellec
+#SBATCH --time=24:00:00
 #SBATCH --job-name=shi_subjlevel
-#SBATCH --mem=12G
+#SBATCH --mem=32G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=8
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/visualization/viz_session-level.py -s $1 -c $2
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/visualization/viz_session-level.py
