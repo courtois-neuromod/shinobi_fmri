683ebb0c2e659ca6a461c5d43bcd47452f92ef82
Yann Harel <yharel109@gmail.com>
2023-05-24 14:51:38 -0400

    created runlevel MVPA

---
+++ shinobi_fmri/mvpa/compute_run_level.py
@@ -0,0 +1,105 @@
+import os
+import os.path as op
+import pandas as pd
+import numpy as np
+import shinobi_behav
+from nilearn.decoding import Decoder
+from sklearn.model_selection import LeaveOneGroupOut, LeavePGroupsOut
+from nilearn.plotting import plot_stat_map, show
+import argparse
+from sklearn.metrics import confusion_matrix
+import seaborn as sbn
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-s",
+    "--subject",
+    default=None,
+    type=str,
+    help="Subject to process",
+)
+args = parser.parse_args()
+
+def main():
+    path_to_data = shinobi_behav.DATA_PATH
+    models = ["full", "simple"]
+    CONDS_LIST = ['HIT', 'JUMP', 'DOWN', 'LEFT', 'RIGHT', 'Kill', 'HealthLoss']#, 'Kill', 'HealthLoss']#'HealthGain', 'UP']
+    #additional_contrasts = ['HIT+JUMP-RIGHT-LEFT-UP-DOWN', 'RIGHT+LEFT+UP+DOWN-HIT-JUMP']
+    contrasts = CONDS_LIST# + additional_contrasts
+    if args.subject is not None:
+        subjects = [args.subject]
+    else:
+        subjects = shinobi_behav.SUBJECTS
+    for sub in subjects:
+        mask_fname = op.join(
+            path_to_data,
+            "cneuromod.processed",
+            "smriprep",
+            sub,
+            "anat",
+            f"{sub}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
+        )
+        anat_fname = op.join(
+            path_to_data,
+            "cneuromod.processed",
+            "smriprep",
+            sub,
+            "anat",
+            f"{sub}_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz",
+        )
+        for model in models:
+            z_maps = []
+            contrast_label = []
+            group_label = []
+            for contrast in contrasts:
+                z_maps_fpath = op.join(path_to_data, "processed", "z_maps", "run-level", contrast)
+                for z_map_fname in os.listdir(z_maps_fpath):
+                    if model in z_map_fname:
+                        if sub in z_map_fname:
+                            session = z_map_fname.split("_")[1]
+                            run = z_map_fname.split("_")[2]
+                            z_maps.append(op.join(z_maps_fpath, z_map_fname))
+                            contrast_label.append(contrast)
+                            group_label.append(session + "_" + run)
+
+
+            decoder = Decoder(estimator='svc', mask=mask_fname, standardize=False,
+                            screening_percentile=5, cv=LeavePGroupsOut(n_groups=5), n_jobs=-1, verbose=1)
+            decoder.fit(z_maps, contrast_label, groups=group_label)
+
+            classification_accuracy = np.mean(list(decoder.cv_scores_.values()))
+            chance_level = 1. / len(np.unique(contrast_label))
+            print(f'Decoding : {sub} {model}')
+            print('Classification accuracy: {:.4f} / Chance level: {}'.format(
+                classification_accuracy, chance_level))
+            
+            for cond in contrasts:
+                output_fname = op.join("./", "reports", "figures", "subject-level", cond, "MVPA_fromruns", f"{sub}_{cond}_{model}_mvpa.png")
+                os.makedirs(op.join("./", "reports", "figures", "subject-level", cond, "MVPA_fromruns"), exist_ok=True)
+                weight_img = decoder.coef_img_[cond]
+                plot_stat_map(weight_img, bg_img=anat_fname, title=f"SVM weights {cond}", output_file=output_fname)
+
+            # Generate confusion matrices across folds
+            confusion_matrices = []
+            for train, test in decoder.cv.split(z_maps, contrast_label, groups=group_label):
+                decoder.fit(np.array(z_maps)[train], np.array(contrast_label)[train], groups=np.array(group_label)[train])
+                y_pred = decoder.predict(np.array(z_maps)[test])
+                y_true = np.array(contrast_label)[test]
+                confusion_mat = confusion_matrix(y_true, y_pred, labels=decoder.classes_)
+                confusion_matrices.append(confusion_mat)
+            averaged_confusion_matrix = np.mean(confusion_matrices, axis=0)
+            std_confusion_matrix = np.std(confusion_matrices, axis=0)
+            sbn.heatmap(averaged_confusion_matrix, annot=True, cmap='Blues', fmt='g', xticklabels=decoder.classes_, yticklabels=decoder.classes_)
+            output_fname = op.join("./", "reports", "figures", "subject-level", "confusion_matrices_fromruns", f"{sub}_{model}_averaged_confusion_matrix.png")
+            os.makedirs(op.join("./", "reports", "figures", "subject-level", "confusion_matrices_fromruns"), exist_ok=True)
+            plt.savefig(output_fname)
+            plt.close()
+            sbn.heatmap(std_confusion_matrix, annot=True, cmap='Blues', fmt='g', xticklabels=decoder.classes_, yticklabels=decoder.classes_)
+            output_fname = op.join("./", "reports", "figures", "subject-level", "confusion_matrices_fromruns", f"{sub}_{model}_std_confusion_matrix.png")
+            plt.savefig(output_fname)
+            plt.close()
+
+
+if __name__ == "__main__":
+    main()
