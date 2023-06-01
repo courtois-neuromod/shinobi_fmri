5f3dab37c7ed44963450e255357e064a22ece4c7
Yann Harel <yharel109@gmail.com>
2023-06-01 16:56:14 -0400

    removed junk

--- slurm/subm_script.py
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=rrg-pbellec
-#SBATCH --time=12:00:00
-#SBATCH --job-name=shinobi_script
-#SBATCH --mem=32G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
-
-/home/hyruuk/python_envs/shinobi_env/bin/python $1
