e19d511a3ebece5bc7fd70b67dbc32ffe6d2bca8
Yann Harel <yharel109@gmail.com>
2023-06-16 14:01:40 -0400

    added arg to batch mvpa

--- slurm/slurm_submission_mvpa_ses-level.sh
+++ slurm/slurm_submission_mvpa_ses-level.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_subject_level.py
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_subject_level.py -s $1
