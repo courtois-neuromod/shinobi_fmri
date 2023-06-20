90ea7afe3cb72fd5c9b8472a09b52f0ec7dc1e4b
Yann Harel <yharel109@gmail.com>
2023-06-19 20:29:03 -0400

    fixed slurmfiles names

--- slurm/batch_launch_sub-level.py
+++ slurm/batch_launch_sub-level.py
@@ -5,7 +5,7 @@ from shinobi_behav import DATA_PATH
 
 for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
     for condition in ['HIT', 'JUMP', 'DOWN', 'HealthLoss', 'Kill', 'LEFT', 'RIGHT', 'UP', 'lvl1', 'lvl4', 'lvl5']:
-        os.system(f"sbatch ./slurm/slurm_submission_subject-level.sh {sub} {condition}")
+        os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}")
         if 'level' not in condition:
             for level in ['lvl1', 'lvl4', 'lvl5']:
-                os.system(f"sbatch ./slurm/slurm_submission_subject-level.sh {sub} {condition}X{level}")
+                os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}X{level}")
