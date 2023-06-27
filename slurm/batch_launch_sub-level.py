1e1725110e3ba989f04d4d1cc42b6e7e8af61eb2
Yann Harel <yharel109@gmail.com>
2023-06-27 14:08:23 -0400

    fixed batch script

--- slurm/batch_launch_sub-level.py
+++ slurm/batch_launch_sub-level.py
@@ -6,6 +6,6 @@ from shinobi_behav import DATA_PATH
 for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
     for condition in ['HIT', 'JUMP', 'DOWN', 'HealthLoss', 'Kill', 'LEFT', 'RIGHT', 'UP', 'lvl1', 'lvl4', 'lvl5']:
         os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}")
-        if 'level' not in condition:
+        if 'lvl' not in condition:
             for level in ['lvl1', 'lvl4', 'lvl5']:
                 os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}X{level}")
