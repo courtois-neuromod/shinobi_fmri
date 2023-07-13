82cd8010325093529689fe1e885b4ff1a29cf16b
Yann Harel <yharel109@gmail.com>
2023-07-13 11:23:13 -0400

    removed level from batch subjlevel

--- slurm/batch_launch_sub-level.py
+++ slurm/batch_launch_sub-level.py
@@ -4,8 +4,8 @@ import glob
 from shinobi_behav import DATA_PATH
 
 for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
-    for condition in ['HIT', 'JUMP', 'DOWN', 'HealthLoss', 'Kill', 'LEFT', 'RIGHT', 'UP', 'lvl1', 'lvl4', 'lvl5']:
+    for condition in ['HIT', 'JUMP', 'DOWN', 'HealthLoss', 'Kill', 'LEFT', 'RIGHT', 'UP']:
         os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}")
-        if 'lvl' not in condition:
-            for level in ['lvl1', 'lvl4', 'lvl5']:
-                os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}X{level}")
+        #if 'lvl' not in condition:
+        #    for level in ['lvl1', 'lvl4', 'lvl5']:
+        #        os.system(f"sbatch ./slurm/subm_subject-level.sh {sub} {condition}X{level}")
