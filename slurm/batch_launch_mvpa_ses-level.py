622a1d346ad5b8cb4034ef60687e55dc96a995cd
Yann Harel <yharel109@gmail.com>
2023-07-16 17:17:17 -0400

    fixed slurmfile

--- slurm/batch_launch_mvpa_ses-level.py
+++ slurm/batch_launch_mvpa_ses-level.py
@@ -4,4 +4,4 @@ import glob
 from shinobi_behav import DATA_PATH
 
 for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
-        os.system(f"sbatch ./slurm/slurm_submission_mvpa_ses-level.sh {sub}")
+        os.system(f"sbatch ./slurm/subm_mvpa_ses-level.sh {sub}")
