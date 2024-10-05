ee3c96d5015007e8aab3c334155d37fcdce2a099
Yann Harel <yharel109@gmail.com>
2024-10-05 11:41:19 -0400

    updated MVPA with hcp

--- shinobi_fmri/mvpa/compute_ses-level_with_hcp.py
+++ shinobi_fmri/mvpa/compute_ses-level_with_hcp.py
@@ -12,6 +12,10 @@ import seaborn as sbn
 import matplotlib.pyplot as plt
 import pickle
 import os.path as op
+import nibabel as nib
+import numpy as np
+from nilearn import image
+from nilearn.input_data import NiftiMasker
 import os
 
 parser = argparse.ArgumentParser()
@@ -35,6 +39,69 @@ if args.subject is not None:
     subjects = [args.subject]
 else:
     subjects = shinobi_behav.SUBJECTS
+
+
+
+def create_common_masker(path_to_data, subjects, masker_kwargs=None):
+    """
+    Create a common NiftiMasker by resampling subject-specific brain masks to a common space.
+
+    Parameters
+    ----------
+    path_to_data : str
+        The base directory where the data is stored.
+    subjects : list of str
+        A list of subject identifiers (e.g., ['sub-01', 'sub-02']).
+    masker_kwargs : dict, optional
+        Additional keyword arguments to pass to the NiftiMasker.
+
+    Returns
+    -------
+    masker : NiftiMasker
+        A fitted NiftiMasker object using the resampled masks.
+    """
+    if masker_kwargs is None:
+        masker_kwargs = {}
+
+    mask_files = []
+    for sub in subjects:
+        mask_fname = op.join(
+            path_to_data,
+            "cneuromod.processed",
+            "smriprep",
+            sub,
+            "anat",
+            f"{sub}_space-MNI152NLin6Asym_desc-brain_mask.nii.gz",
+        )
+        shinobi_fname = op.join(
+            path_to_data,
+            "shinobi.fmriprep",
+            sub,
+            "ses-005",
+            "func",
+            f"{sub}_ses-005_task-shinobi_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz",
+        )
+        # Load and resample anatomical mask to functional image space
+        shinobi_raw_img = nib.load(shinobi_fname)
+        aff_orig = shinobi_raw_img.affine[:, -1]
+        target_affine = np.column_stack([np.eye(4, 3) * 4, aff_orig])
+        target_shape = shinobi_raw_img.shape[:3]
+        mask_resampled = image.resample_img(
+            mask_fname,
+            target_affine=target_affine,
+            target_shape=target_shape
+        )
+        mask_files.append(mask_resampled)
+
+    masker = NiftiMasker(**masker_kwargs)
+    masker.fit(mask_files)
+    return masker
+
+
+all_subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
+masker = create_common_masker(path_to_data, all_subjects)
+target_affine, target_shape = masker.target_affine, masker.target_shape
+
 for sub in subjects:
     mvpa_results_path = op.join(path_to_data, "processed", "mvpa_results")
     os.makedirs(mvpa_results_path, exist_ok=True)
@@ -64,15 +131,31 @@ for sub in subjects:
         z_maps = []
         contrast_label = []
         session_label = []
+
         for contrast in contrasts:
             z_maps_fpath = op.join(path_to_data, "processed", "z_maps", "ses-level", contrast)
             for z_map_fname in os.listdir(z_maps_fpath):
                 if model in z_map_fname:
                     if sub in z_map_fname:
                         session = z_map_fname.split("_")[1]
-                        z_maps.append(op.join(z_maps_fpath, z_map_fname))
+                        niimap = image.load_img(op.join(z_maps_fpath, z_map_fname))
+                        resampled_img = image.resample_img(niimap, target_affine=target_affine, target_shape=target_shape)
+                        z_maps.append(masker.transform(resampled_img))
                         contrast_label.append(contrast)
                         session_label.append(session)
+
+        subfolder = op.join(path_to_data, "hcp_results", sub)
+        runfolders = [f for f in os.listdir(subfolder) if 'run-' in f]
+        for runfolder in runfolders:
+            conditions = [f.split('.')[0] for f in os.listdir(op.join(subfolder, runfolder, 'z_score_maps')) if '.nii.gz' in f]
+            for cond in conditions:
+                fpath = op.join(subfolder, runfolder, 'z_score_maps', '{}.nii.gz'.format(cond))
+                print(f'Loading {fpath}')
+                niimap = image.load_img(fpath)
+                resampled_img = image.resample_img(niimap, target_affine=target_affine, target_shape=target_shape)
+                z_maps.append(masker.transform(resampled_img))
+                session_label.append('_'.join(runfolder.split('_')[2:]))
+                contrast_label.append(cond)
 
         decoder = Decoder(estimator='svc', mask=mask_fname, standardize=False, scoring='accuracy',
                         screening_percentile=5, cv=LeaveOneGroupOut(), n_jobs=-1, verbose=1)
@@ -84,7 +167,7 @@ for sub in subjects:
         print('Classification accuracy: {:.4f} / Chance level: {}'.format(
             classification_accuracy, chance_level))
         
-        for cond in contrasts:
+        for cond in np.unique(contrast_label):
             output_fname = op.join("./", "reports", "figures", "ses-level", cond, "MVPA", f"{sub}_{cond}_{model}_mvpa.png")
             os.makedirs(op.join("./", "reports", "figures", "ses-level", cond, "MVPA"), exist_ok=True)
             weight_img = decoder.coef_img_[cond]
