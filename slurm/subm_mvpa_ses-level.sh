f49c2d1d9ee7c95eab3f430f5a95a14253cfeb35
Yann Harel <yharel109@gmail.com>
2025-02-14 18:05:24 -0500

    adjusted slurmfile

--- slurm/subm_mvpa_ses-level.sh
+++ slurm/subm_mvpa_ses-level.sh
@@ -4,6 +4,6 @@
 #SBATCH --job-name=shi_mvpa_seslvl
 #SBATCH --mem=256G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=40
 
 /home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_ses-level_with_hcp.py -s $1
