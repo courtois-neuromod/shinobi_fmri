07c44df5215291003a106b03639c23918877154d
Yann Harel <yharel109@gmail.com>
2025-02-14 21:28:34 -0500

    fixed batch launch

--- slurm/subm_mvpa_ses-level.sh
+++ slurm/subm_mvpa_ses-level.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
 
-/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_ses-level_with_hcp.py -s $1
+/home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_ses-level_with_hcp.py -s $1 --task $2 --perm-index $3
