8a4022e9fe62c670c4abfc9dab2a957f5afec865
Yann Harel <yharel109@gmail.com>
2025-02-23 13:04:47 -0500

    changed alloc

--- slurm/subm_mvpa_ses-level.sh
+++ slurm/subm_mvpa_ses-level.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-pbellec
-#SBATCH --time=3:00:00
+#SBATCH --time=12:00:00
 #SBATCH --job-name=shi_mvpa_seslvl
-#SBATCH --mem=16G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=8
 
 /home/hyruuk/python_envs/shinobi_env/bin/python /home/hyruuk/projects/def-pbellec/hyruuk/shinobi_fmri/shinobi_fmri/mvpa/compute_ses-level_with_hcp.py -s $1 --task $2 --perm-index $3
