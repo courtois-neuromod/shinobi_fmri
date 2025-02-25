cb5ecef7b25aeffa30ac1b6e42367181b3ffc002
Yann Harel <yharel109@gmail.com>
2025-02-25 16:45:33 -0500

    adjusted number of jobs

--- slurm/batch_launch_mvpa_ses-level.py
+++ slurm/batch_launch_mvpa_ses-level.py
@@ -5,5 +5,5 @@ from shinobi_behav import DATA_PATH
 
 for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
         os.system(f"sbatch ./slurm/subm_mvpa_ses-level.sh {sub} classif 1")
-        for perm_index in range(0, 1000, 10):
+        for perm_index in range(0, 1000, 4):
                 os.system(f"sbatch ./slurm/subm_mvpa_ses-level.sh {sub} perm {perm_index}")
