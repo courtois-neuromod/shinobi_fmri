6a66bcc4c829bed1dff86d6135850bd71bd62d88
Notebook User <notebook@example.com>
2025-12-19 01:14:40 -0500

    removed junk

--- slurm/batch_launch_corrmat_chunks.py
+++
@@ -1,9 +0,0 @@
-import os
-
-NUM_JOBS = 100
-CHUNK_SIZE = 100
-SUBMISSION_SCRIPT = "./slurm/subm_corrmat_chunk.sh"
-
-for job_idx in range(NUM_JOBS):
-    chunk_start = job_idx * CHUNK_SIZE
-    os.system(f"sbatch {SUBMISSION_SCRIPT} {chunk_start}")
--- slurm/batch_launch_mvpa_ses-level.py
+++
@@ -1,9 +0,0 @@
-import os
-import os.path as op
-import glob
-from shinobi_fmri.config import DATA_PATH
-
-for sub in ["sub-01", "sub-02", "sub-04", "sub-06"]:
-        os.system(f"sbatch ./slurm/subm_mvpa_ses-level.sh {sub} classif")
-        for perm_index in range(0, 1000, 10):
-                os.system(f"sbatch ./slurm/subm_mvpa_ses-level.sh {sub} perm {perm_index}")
